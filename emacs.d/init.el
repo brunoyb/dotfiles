367472941c22293caa104c63e8dae37bd7abc0cc
Bruno Baldochi <brunoyb@gmail.com>
2016-04-17 19:49:00 -0300

    Update package-archives URLs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -42,12 +42,11 @@
       kept-old-versions 2
       version-control t)
 
+(require 'package)
 (setq package-enable-at-startup nil)
-(setq package-archives
-      '(("gnu"         . "http://elpa.gnu.org/packages/")
-        ("marmalade"   . "http://marmalade-repo.org/packages/")
-        ("melpa"       . "http://melpa.milkbox.net/packages/")
-        ("org"         . "http://orgmode.org/elpa/")))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 ; defvar is the correct way to declare global variables
