63a818f92ae45be5962a28f34313ec829b42c9c1
Bruno Baldochi <brunoyb@gmail.com>
2019-06-14 03:38:03 -0300

    Remove readline config for powershell
    Defaults are fine.

--- powershell_profile.d/30-readline.ps1
+++
@@ -1,3 +0,0 @@
-﻿Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
